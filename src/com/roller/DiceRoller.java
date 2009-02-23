e38fb16f076cd8872c55acd2b211730980244205
Matt Fowles <fowles@frankenwuzzle.(none)>
2009-02-22 23:45:01 -0500

    add support for dynamically adding items to the list refactor all of the ids used by different widgets into different groups add a dialog for specifiying roll details

--- src/com/roller/DiceRoller.java
+++ src/com/roller/DiceRoller.java
@@ -1,17 +1,22 @@
 package com.roller;
 
 import android.app.Activity;
+import android.app.Dialog;
 import android.content.Context;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.View.OnClickListener;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.ListView;
 import android.widget.TextView;
 
 public class DiceRoller extends Activity implements View.OnClickListener {
     private ListAdapter rollAdapter = null;
+    private ListView listView = null;
 
     /** Called when the activity is first created. */
     @Override
@@ -22,39 +27,34 @@ public class DiceRoller extends Activity implements View.OnClickListener {
 
         final View main = inflater.inflate(R.layout.main, null);
 
-        final View addDiceButton = main.findViewById(R.id.add_dice);
+        final View addDiceButton = main.findViewById(R.main.add_dice);
         addDiceButton.setOnClickListener(this);
 
         final Context ctxt = getApplicationContext();
-        final ListView listView = (ListView) main.findViewById(R.id.list);
+        listView = (ListView) main.findViewById(R.main.list);
         rollAdapter = new ListAdapter(ctxt);
         listView.setAdapter(rollAdapter);
-        rollAdapter.add(new RollInfo("Attack", 10));
-        rollAdapter.add(new RollInfo("Damage", 20));
-
         setContentView(main);
     }
 
     private static class RollInfo {
-        private final String name;
+        private final CharSequence name;
         private final int numDice;
+        private final boolean isDamage;
 
-        public RollInfo(final String n, final int d) {
+        public RollInfo(final CharSequence n, final int d, final boolean damage) {
             name = n;
             numDice = d;
+            isDamage = damage;
         }
 
-        public String getName() {
-            return name;
-        }
-
-        public int getNumDice() {
-            return numDice;
-        }
+        public CharSequence getName() { return name; }
+        public int getNumDice() { return numDice; }
+        public boolean isDamage() { return isDamage; }
 
         public void updateView(final View v) {
-            final TextView nameView = (TextView) v.findViewById(R.id.item_name);
-            final TextView diceView = (TextView) v.findViewById(R.id.item_dice);
+            final TextView nameView = (TextView) v.findViewById(R.item.name);
+            final TextView diceView = (TextView) v.findViewById(R.item.dice);
             nameView.setText(name);
             diceView.setText(numDice + "D10");
         }
@@ -62,12 +62,11 @@ public class DiceRoller extends Activity implements View.OnClickListener {
 
     private class ListAdapter extends ArrayAdapter<RollInfo> {
         public ListAdapter(final Context context) {
-            super(context, R.layout.item, R.id.item_dummy);
+            super(context, R.layout.item, R.item.dummy);
         }
 
         @Override
-        public View getView(final int position, final View convertView,
-                final ViewGroup parent) {
+        public View getView(final int position, final View convertView, final ViewGroup parent) {
             final View res = super.getView(position, convertView, parent);
             getItem(position).updateView(res);
             return res;
@@ -76,6 +75,33 @@ public class DiceRoller extends Activity implements View.OnClickListener {
     }
 
     public void onClick(final View v) {
-        rollAdapter.add(new RollInfo("Damage", 20));
+        final Dialog d = new Dialog(this);
+        d.setTitle("Add Roll Option");
+        d.setContentView(R.layout.add_item);
+
+        final Button ok = (Button) d.findViewById(R.add_item.ok);
+        ok.setOnClickListener(new OnClickListener() {
+            public void onClick(final View v) {
+                final TextView name = (TextView) d.findViewById(R.add_item.name);
+                final TextView dice = (TextView) d.findViewById(R.add_item.dice);
+                final CheckBox damage = (CheckBox) d.findViewById(R.add_item.damage);
+
+                rollAdapter.add(new RollInfo(
+                        name.getText(),
+                        Integer.parseInt(dice.getText().toString()),
+                        damage.isChecked()
+                ));
+                d.dismiss();
+            }
+        });
+
+        final Button cancel = (Button) d.findViewById(R.add_item.cancel);
+        cancel.setOnClickListener(new OnClickListener() {
+            public void onClick(final View v) {
+                d.dismiss();
+            }
+        });
+
+        d.show();
     }
 }
